e3f2c13cdc832dd5de19232f6a51e394b6fcc49a
Ricardo Mateus <ricardomateus23@hotmail.com>
2019-06-20 17:51:35 -0300

    subindo primeiros arquivos

---
+++ meadowlark.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+
+//set up handlebars view engine
+var handlebars = require('express3-handlebars').create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+app.get('/about', function(req, res){
+	res.render('about');
+});
+
+//custom page
+app.use(function(req, res){
+	res.status(404);
+	res.render('404');
+});
+
+//custom 500 page
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+	console.log('Express started on http://localhost:' +
+			   app.get('port') + '; press Ctrl-C to terminate.');
+});
