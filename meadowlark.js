33342b2e33237cb24bad9bb532e1386418ec7117
Ricardo Mateus <ricardomateus23@hotmail.com>
2019-06-21 22:03:31 -0300

    criacao de midleware para caminho estatico e exemplo de uso de conteudo dinamico

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+var fortunes = [
+		"Conquer your fears or they will conquer you.",
+		"Rivers need springs.",
+		"Do not fear what you don't know.",
+		"You will have a pleasant surprise.",
+		"Whenever possible, keep it simple.",
+];
+
 //set up handlebars view engine
 var handlebars = require('express3-handlebars').create({ defaultLayout: 'main' });
 app.engine('handlebars', handlebars.engine);
@@ -9,12 +17,15 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
 	res.render('home');
 });
 
 app.get('/about', function(req, res){
-	res.render('about');
+	var randomFortune =	fortunes[Math.floor(Math.random() * fortunes.length)];
+	res.render('about', { fortune: randomFortune });
 });
 
 //custom page
@@ -31,6 +42,7 @@ app.use(function(err, req, res, next){
 });
 
 app.listen(app.get('port'), function(){
+	console.log(__dirname + '/public');
 	console.log('Express started on http://localhost:' +
 			   app.get('port') + '; press Ctrl-C to terminate.');
 });
